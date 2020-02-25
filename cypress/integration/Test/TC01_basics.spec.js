5a6893fa020ba376317ca045a331feacdd1c5dc8
TusharShukla <61314138+TusharShukla@users.noreply.github.com>
2020-02-25 13:27:34 +0530

    Update TC01_basics.spec.js

--- cypress/integration/Test/TC01_basics.spec.js
+++ cypress/integration/Test/TC01_basics.spec.js
@@ -3,7 +3,7 @@
 describe('contact_us', function(){
     it('Contact', function(){
         cy.visit('https://bombinate.com/login?ow=/')
-        cy.get('#cy-login-email').type('tushar.shukla@fusionfirst.co.in')
+        cy.get('#cy-login-email').type('tushar.shukla@fusionfrst.co.in')
         cy.get('#cy-login-password').type('12345678')
         cy.get('#cy-login-button').click()
     })
